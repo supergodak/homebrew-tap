ab97e9288d6491217f64a7f7a36a19d9f5d3db3c
supergodak <85863833+supergodak@users.noreply.github.com>
2026-07-22 09:05:15 +0900

    tameo: update to 0.1.12

--- Casks/tameo.rb
+++ Casks/tameo.rb
@@ -1,6 +1,6 @@
 cask "tameo" do
-  version "0.1.11"
-  sha256 "6d9144a48994878731e2b8c0a5c530b7b1ec8d06239280e671f1daaba9e23e73"
+  version "0.1.12"
+  sha256 "59b55cc2a1513a868f4cf69f6aa6d53ff3829da198fadd05b328cedf64a1bfcd"
 
   url "https://github.com/supergodak/tameo/releases/download/v#{version}/Tameo.dmg",
       verified: "github.com/supergodak/tameo/"
